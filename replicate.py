2372b1a732030c5185a9a675a6042a2dca19ad49
abernhardt <aaronbernhardt.scull@gmail.com>
2017-11-27 11:05:13 -0500

    Update replicate.py
    Updated GUI. Quit button now works and submit currently prints to console.
    Need to modify so that submit sends the login info to TM1Service

--- replicate.py
+++ replicate.py
@@ -1,22 +1,23 @@
-#!/usr/bin/env python
+#Get target cube and dimensions
+
 
 import tkinter as tk
+
+
 #GUI for user input to login to the TM1 server.
 #Should be used as the first user-facing side of the application.
 #Currently, I just want to focus on getting replicate to work. Then,
-# I would like to have a menu selection with different actions for TM1 that
-# are not exactly native to TM1.
+#I would like to have a toolkit that has multiple useful TM1 tools that
+#may or may not be readily available in TM1 Architect.
 class Application(tk.Frame):
 
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
         self.createWidgets()
         self.grid()
-        
+
     def createWidgets(self):
         top = self.winfo_toplevel()
-        v = tk.IntVar()
-
         top.rowconfigure(0, weight=1)
         top.columnconfigure(0, weight=1)
 
@@ -40,40 +41,27 @@ class Application(tk.Frame):
         self.TM1PasswordLabel.grid(column=1, row=4)
         self.TM1PasswordEntry.grid(column=10, row=4)
 
+        self.sslvar=tk.BooleanVar(value=True)
         self.TM1SSLLabel=tk.Label(self, text="Using SSL?")
-        self.TM1SSLFALSE=tk.Radiobutton(self, text="False", variable=v, value=0)
-        self.TM1SSLTRUE=tk.Radiobutton(self, text="True", variable=v, value=1)
-        self.TM1SSLTRUE.deselect()
-        self.TM1SSLFALSE.deselect()
+        self.TM1SSLFALSE=tk.Radiobutton(self, text="False", variable=self.sslvar, value=False)
+        self.TM1SSLTRUE=tk.Radiobutton(self, text="True", variable=self.sslvar, value=True)
         self.TM1SSLLabel.grid(column=1, row=5)
-        self.TM1SSLFALSE.grid(column=10, row=8)
-        self.TM1SSLTRUE.grid(column=10, row=7)
-        ### Set Radio Buttons default to off-set
+        self.TM1SSLFALSE.grid(column=7, row=6)
+        self.TM1SSLTRUE.grid(column=7, row=7)
 
-        if self.TM1SSLTRUE.select():
-            self.TM1SSLFALSE.deselect()
-        if self.TM1SSLFALSE.select():
-            self.TM1SSLTRUE.deselect()
+        self.submitButton=tk.Button(self, text="Submit", command=self.submitButton)
+        self.submitButton.grid(column=10, row=9)
+
+        self.quitButton=tk.Button(self, text="Quit", command=self.quit)
+        self.quitButton.grid(column=1, row = 9)
+
+    def submitButton(self):
+        print(self.TM1AddressEntry.get())
+        print(self.TM1PortEntry.get())
+        print(self.TM1UsernameEntry.get())
+        print(self.TM1PasswordEntry.get())
+        print("%r" % (self.sslvar.get()))
+
 app = Application()
 app.master.title=('Sample Application')
 app.mainloop()
-
-"""def GetUserInput():
-    with f as
-
-
-
-from TM1py.Services import TM1Service
-with TM1Service(address='localhost', port=8002, user='admin', password='', ssl=True) as tm1:
-
-    cubelist=tm1.cubes.get_all_names()
-    for cube in cubelist:
-        print ("Cube name: ", cube)
-
-
-# Get directory of TM1 instances
-# The variable 'name', here, should be selected from a VBS script or something and fed as such.
-
-print ('Named explicitly:')
-for name in glob.glob('C:\TM1/*'):
-    print ('', name)"""
